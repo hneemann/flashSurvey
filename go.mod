50ac6c7a78f83a31583048a593ddecead97fd71a
hneemann <helmut.neemann@arcor.de>
2025-08-11 08:11:41 +0200

    added a sync test

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module flashSurvey
 
 go 1.24
 
-require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+require (
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ survey/data_test.go
@@ -0,0 +1,60 @@
+package survey
+
+import (
+	"sync"
+	"testing"
+)
+import "github.com/stretchr/testify/assert"
+
+// TestSync tests if the Surveys struct can handle concurrent voting without data
+// races or inconsistencies.
+// It creates 100 goroutines, each creating a survey and simulating 100 users
+// voting on a survey.
+// Run with `go test -race` to check for data races.
+func TestSync(t *testing.T) {
+	s := New("localhost", 30, false, false)
+	wg := &sync.WaitGroup{}
+	for range 100 {
+		wg.Add(1)
+		go voting(t, s, wg)
+	}
+	wg.Wait()
+}
+
+const voters = 100
+
+func voting(t *testing.T, s *Surveys, mainWg *sync.WaitGroup) {
+	userId := UserId(RandomString())
+	description := SurveyQuestion{
+		Title:    "Test",
+		Options:  []string{"Yes", "No"},
+		Multiple: false,
+	}
+	sid, err := s.New(userId, "", description)
+	assert.NoError(t, err)
+
+	start := make(chan struct{})
+	wg := sync.WaitGroup{}
+	for range voters {
+		wg.Add(1)
+		go func() {
+			uid := UserId(RandomString())
+			<-start
+			err := s.Vote(sid, uid, []int{0}, 1)
+			assert.NoError(t, err)
+			wg.Done()
+		}()
+	}
+
+	close(start)
+	wg.Wait()
+
+	err = s.Uncover(userId, sid)
+	assert.NoError(t, err)
+
+	r := s.GetResult(userId, sid)
+	assert.NotNil(t, r)
+
+	assert.EqualValues(t, voters, r.Votes)
+	mainWg.Done()
+}
